31c74375b12cc547312a6551f969908dc356b266
Riley Champion <rchamp2u@gmail.com>
2024-05-12 00:08:13 -0400

    Add logo svg and add start to add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+
 import LandingPage from './views/LandingPage.tsx';
+import {Container, Navbar} from 'react-bootstrap'
+import logo from './resources/riley.svg'
 
 function App() {
   return (
     <div className="App">
+      <Navbar variant='dark'>
+        <Container>
+          <Navbar.Brand href="#landing">
+            <img width="30" height="30" src={logo} alt="Riley Champion Logo"/>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
       <LandingPage/>
     </div>
   );
