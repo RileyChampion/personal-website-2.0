cca58eeb63dadb9c1cff73642f48ce193b3e55d1
Riley Champion <rchamp2u@gmail.com>
2024-05-18 11:32:09 -0400

    Remove navbar, change positioning of rays

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import logo from './resources/riley.svg'
 function App() {
   return (
     <div className="App">
-      <Navbar variant='dark'>
-        <Container>
-          <Navbar.Brand href="#landing">
-            <img width="30" height="30" src={logo} alt="Riley Champion Logo"/>
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
       <LandingPage/>
     </div>
   );
