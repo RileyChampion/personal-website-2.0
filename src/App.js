07224fa6cad548ffae642178d3a344d927541466
Riley Champion <rchamp2u@gmail.com>
2024-07-22 22:21:14 -0400

    CSS for about me title and content boxes. filler code for site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import LandingPage from './views/LandingPage.tsx';
 import {Container, Navbar} from 'react-bootstrap'
+import AboutMe from './views/AboutMe.tsx';
 
 function App() {
   return (
     <div className="App">
-      
       <LandingPage/>
+      <AboutMe />
     </div>
   );
 }
--- src/views/AboutMe.tsx
+++ src/views/AboutMe.tsx
@@ -1,31 +1,44 @@
 import React from "react";
 import './styles/about-me.scss'
 
-
-
 function AboutMe() {
-    <section id="about-me">
-        <div id='about-me-container'>
-            <div className='about-me-title'>
-                <h2>ABOUT ME</h2>
-            </div>
-            <div id="about-me-content">
-                <div className="about-me-content-image-box">
-                    <img className="about-me-content-image" />
+    return(
+        <section id="about-me">
+            <div id='about-me-container'>
+                <div id='about-me-title'>
+                    <h2>ABOUT ME</h2>
                 </div>
-                <div className="about-me-content-info-box">
-                    <div className="about-me-content-info">
-                        <h4>Hey! I'm Riley 👋🏼</h4>
-                        <p>Lorem ipsum</p>
-                        <div className="about-me-content-info-buttons-container">
-                            <button className="about-me-content-info-button">Resume</button>
-                            <button className="about-me-content-info-button">LinkedIn</button>
+                <div id="about-me-content">
+                    <div className="about-me-content-image-box">
+                        <img className="about-me-content-image" />
+                    </div>
+                    <div className="about-me-content-info-box">
+                        <div className="about-me-content-info">
+                            <h4>Hey! I'm Riley 👋🏼</h4>
+                            <p>Esse dolor eu aliqua consectetur esse aliquip. Et officia ut sit sint ullamco reprehenderit irure amet quis. Nulla labore magna ea aute officia irure aliqua proident anim ipsum. Dolore est nulla aute cupidatat. Incididunt amet sit do adipisicing. Culpa adipisicing quis eiusmod est anim incididunt eiusmod cupidatat amet magna.</p>
+                            <p>Esse dolor eu aliqua consectetur esse aliquip. Et officia ut sit sint ullamco reprehenderit irure amet quis. Nulla labore magna ea aute officia irure aliqua proident anim ipsum. Dolore est nulla aute cupidatat. Incididunt amet sit do adipisicing. Culpa adipisicing quis eiusmod est anim incididunt eiusmod cupidatat amet magna.</p>
+                            <div className="about-me-content-info-buttons-container">
+                                <button 
+                                    className="about-me-content-info-button"
+                                    onClick={()=> window.open(process.env.PUBLIC_URL + "/Riley_Champion_Resume.pdf", "_blank")}
+                                >
+                                    Resume
+                                </button>
+                                <button 
+                                    className="about-me-content-info-button"
+                                    onClick={()=> window.open("https://www.linkedin.com/in/rileychampion/", "_blank")}
+                                >
+                                    LinkedIn
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    )
 }
 
 export default AboutMe;
+
+/* Rectangle 53 */
