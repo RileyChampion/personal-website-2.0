c8a0e37a9bde09990502d65523c6b2959bfd810c
Riley Champion <rchamp2u@gmail.com>
2024-08-03 23:17:22 -0400

    Addeded FadeInSection component to reveal new sections

---
+++ src/components/FadeInSection.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useRef, useState} from 'react';
+import '../animations.scss'
+
+interface FadeInSectionProps {
+    children: React.ReactNode;
+    fadeIn: string
+    floatUp: string
+    customStyle: React.CSSProperties
+}
+
+function FadeInSection({children, fadeIn, floatUp, customStyle} : FadeInSectionProps){
+    const [isVisible, setIsVisible] = useState(false);
+    const domRef = useRef<HTMLDivElement | null>(null);
+
+    console.log(customStyle)
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                setIsVisible(true);
+                if (domRef.current) {
+                    observer.unobserve(domRef.current);
+                }
+            }
+            });
+        });
+
+        if (domRef.current) {
+            observer.observe(domRef.current);
+        }
+
+        return () => {
+            if (domRef.current) {
+            observer.unobserve(domRef.current);
+            }
+        };
+
+    }, []);
+
+    return (
+    <div style={isVisible ? {animation: `fade-in ${fadeIn} ease-in-out, float-up ${floatUp} ease-in-out`, ...customStyle} : {}} ref={domRef}>
+        {children}
+    </div>
+    );
+};
+    
+export default FadeInSection;
--- src/views/AboutMe.tsx
+++ src/views/AboutMe.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FadeInSection from "../components/FadeInSection.tsx";
 import './styles/about-me.scss'
 
 function AboutMe() {
@@ -9,34 +10,38 @@ function AboutMe() {
                     <h2>ABOUT ME</h2>
                 </div>
                 <div id="about-me-content">
-                    <div className="about-me-content-image-box">
-                        <img className="about-me-content-image" loading="lazy" src={process.env.PUBLIC_URL + "/riley.jpg"} style={{height: '600px'}} alt="Picture of Riley" />
-                    </div>
-                    <div className="about-me-content-info-box">
-                        <div className="about-me-content-info">
-                            <div className="about-me-content-info-title">
-                                <h4>Hey! I'm Riley 👋🏼</h4>
-                            </div>
-                            <div className="about-me-content-info-text">
-                                <p>Esse dolor eu aliqua consectetur esse aliquip. Et officia ut sit sint ullamco reprehenderit irure amet quis. Nulla labore magna ea aute officia irure aliqua proident anim ipsum. Dolore est nulla aute cupidatat. Incididunt amet sit do adipisicing. Culpa adipisicing quis eiusmod est anim incididunt eiusmod cupidatat amet magna.</p>
-                                <p>Esse dolor eu aliqua consectetur esse aliquip. Et officia ut sit sint ullamco reprehenderit irure amet quis. Nulla labore magna ea aute officia irure aliqua proident anim ipsum. Dolore est nulla aute cupidatat. Incididunt amet sit do adipisicing. Culpa adipisicing quis eiusmod est anim incididunt eiusmod cupidatat amet magna.</p>
-                            </div>
-                            <div className="about-me-content-info-buttons-container">
-                                <button 
-                                    className="about-me-content-info-button"
-                                    onClick={()=> window.open(process.env.PUBLIC_URL + "/Riley_Champion_Resume.pdf", "_blank")}
-                                >
-                                    Resume
-                                </button>
-                                <button 
-                                    className="about-me-content-info-button"
-                                    onClick={()=> window.open("https://www.linkedin.com/in/rileychampion/", "_blank")}
-                                >
-                                    LinkedIn
-                                </button>
+                    <FadeInSection fadeIn="1s" floatUp="1s" customStyle={{zIndex: 10}}>
+                        <div className="about-me-content-image-box">
+                            <img className="about-me-content-image" loading="lazy" src={process.env.PUBLIC_URL + "/riley.jpg"} style={{height: '600px'}} alt="Riley Champion" />
+                        </div>
+                    </FadeInSection>
+                    <FadeInSection fadeIn="1.25s" floatUp="1.25s">
+                        <div className="about-me-content-info-box">
+                            <div className="about-me-content-info">
+                                <div className="about-me-content-info-title">
+                                    <h4>Hey! I'm Riley 👋🏼</h4>
+                                </div>
+                                <div className="about-me-content-info-text">
+                                    <p>Esse dolor eu aliqua consectetur esse aliquip. Et officia ut sit sint ullamco reprehenderit irure amet quis. Nulla labore magna ea aute officia irure aliqua proident anim ipsum. Dolore est nulla aute cupidatat. Incididunt amet sit do adipisicing. Culpa adipisicing quis eiusmod est anim incididunt eiusmod cupidatat amet magna.</p>
+                                    <p>Esse dolor eu aliqua consectetur esse aliquip. Et officia ut sit sint ullamco reprehenderit irure amet quis. Nulla labore magna ea aute officia irure aliqua proident anim ipsum. Dolore est nulla aute cupidatat. Incididunt amet sit do adipisicing. Culpa adipisicing quis eiusmod est anim incididunt eiusmod cupidatat amet magna.</p>
+                                </div>
+                                <div className="about-me-content-info-buttons-container">
+                                    <button 
+                                        className="about-me-content-info-button"
+                                        onClick={()=> window.open(process.env.PUBLIC_URL + "/Riley_Champion_Resume.pdf", "_blank")}
+                                    >
+                                        Resume
+                                    </button>
+                                    <button 
+                                        className="about-me-content-info-button"
+                                        onClick={()=> window.open("https://www.linkedin.com/in/rileychampion/", "_blank")}
+                                    >
+                                        LinkedIn
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </FadeInSection>
                 </div>
             </div>
         </section>
